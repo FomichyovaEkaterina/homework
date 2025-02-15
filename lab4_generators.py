fa3360b7ab6dfc5153254128de23674d60a36562
FomichyovaEkaterina <y_fomichyova@kbtu.kz>
2025-02-15 10:18:16 +0600

    lab4_generators.py

--- lab4_generators.py
+++ lab4_generators.py
@@ -1,25 +1,42 @@
 #Task1
+def square_generator(N):
+    for i in range(N + 1):
+        yield i ** 2
 N = int(input())
-sqr = (i ** 2 for i in range(N + 1))
-print(*sqr)
+for square in square_generator(N):
+    print(square, end=" ")
+print()
 
 #Task2
+def even_generator(N):
+    for i in range(0, N + 1, 2):
+        yield i
 N = int(input())
-even = (str(i) for i in range(0, N + 1, 2))
-print(",".join(even))
+print(",".join(map(str, even_generator(N))))
 
 #Task3
+def divisible(N):
+    for i in range(N + 1):
+        if i % 3 == 0 and i % 4 == 0:
+            yield i
+
 N = int(input())
-num = (i for i in range(N + 1) if i % 3 == 0 and i % 4 == 0)
-print(*num)
+print(list(divisible(N)))
 
 #Task4
+def squares(a, b):
+    for i in range(a, b + 1):
+        yield i ** 2
 a, b = map(int, input().split())
-squares = (i ** 2 for i in range(a, b + 1))
-print(*squares)
+for sq in squares(a, b):
+    print(sq, end=" ")
+print()
 
 #Task5
+def countdown(n):
+    for i in range(n, -1, -1):
+        yield i
 N = int(input())
-down = (i for i in range(N, -1, -1))
-print(*down)
+print(" ".join(map(str, countdown(N))))
+
 
